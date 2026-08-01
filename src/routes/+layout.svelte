78bd8896f6fdfaebc2d11d6a471917df34e74c10
Max G. <max.gottschalk@mailbox.org>
2026-08-01 11:56:11 +0200

    cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import '@fontsource-variable/geist/wght.css';
+	import '@fontsource-variable/geist-mono/wght.css';
+	import { ModeWatcher } from 'mode-watcher';
 	import favicon from '#lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<ModeWatcher />
+
 {@render children()}
